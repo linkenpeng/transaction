7b6764cae1652db3a77cb238e18f1fadd3e98d32
peter <peter.peng@watsons.com.cn>
2019-11-05 11:49:20 +0800

    改成war打包方式

--- src/main/java/com/intecsec/springboot/RunApplication.java
+++ src/main/java/com/intecsec/springboot/RunApplication.java
@@ -1,15 +1,12 @@
 package com.intecsec.springboot;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
-public class RunApplication {
+public class RunApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RunApplication.class, args);
 	}
-
 }
